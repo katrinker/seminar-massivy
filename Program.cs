80bc0a3102976565cd51e2e80439e7c3866f272b
Catherine <katrinker@bk.ru>
2023-12-08 02:00:57 +0700

    solved 4 task

--- Program.cs
+++ Program.cs
@@ -95,43 +95,51 @@ r = max-min;
 //Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
 //Размер массива должен быть равен количеству цифр.
 
+    
 
 Console.WriteLine("Введите число в диапазоне от 1 до 100 000: ");//86 425
 int num = Convert.ToInt32(Console.ReadLine());
 
-int size = 0;
+int size = num.ToString().Length;
+Console.WriteLine(size);
 
-while(num%10 !<=9)
-{
-    size = size +1;
-}
 int[] array = new int[size];
-
-array[size-1] = num%10;
-if (num == 100000)
-{
-    array[0] = num%100000;
-} 
-if (num <= 99999 && num > 9999)
-{
-    array[0] = num%10000;
-}
-if (num <= 9999 && num > 999)
-{
-    array[0] = num%1000;
-}
-if (num <= 999 && num > 99)
-{
-    array[0] = num%100;
-}
-if (num <= 99 && num > 9)
-{
-    array[0] = num%100;
-}
 
 
 for(int i = 0; i<array.Length; i++)
+{   
+
+    if (num == 100000)
 {
+    array[0] = num/100000;
+} 
+
+if (num <= 99999 && num > 9999)
+{
+    array[0] = num/10000;
+    array[1] = num/1000%10;
+    array[2] = num/100%10;
+    array[3] = num/10%10;
+}
+
+if (num <= 9999 && num > 999)
+{
+    array[0] = num/1000;
+    array[1] = num/100%10;
+    array[2] = num/10%10;
+}
+
+if (num <= 999 && num > 99)
+{
+    array[0] = num/100;
+    array[1] = num/10%10;
+}
+
+if (num <= 99 && num > 9)
+{
+    array[0] = num/100;
+}
+array[size-1] = num%10;
     System.Console.Write(array[i]+ ", ");
 
 }
