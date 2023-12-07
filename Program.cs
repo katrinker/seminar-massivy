a3ce3635c49b446cc89cfb8c65aaf53eabbd87bc
Catherine <katrinker@bk.ru>
2023-12-07 22:35:22 +0700

    solved 2 task

--- Program.cs
+++ Program.cs
@@ -16,8 +16,35 @@ count = count + 1;
     }
 
 }
-    System.Console.Write(count);*/
+System.Console.WriteLine();
+System.Console.Write(count " элементов массива, значения которых лежат в отрезке [20,90].");*/
 
 
 
+
+//Задача 2: Задайте массив на 10 целых чисел. 
+//Напишите программу, которая определяет количество чётных чисел в массиве.
+
+
+/*int [] array = new int[10];
+int count = 0;
+
+for(int i = 0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(1,21);  
+    System.Console.Write(array[i]+ " ");
+    
+    if (array[i] % 2 == 0)
+    {
+        count = count + 1;
+    }
+}
+System.Console.WriteLine();
+System.Console.Write(count + " четных чисел в массиве");*/
+
+
+
+
+//Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью.
+// Найдите разницу между максимальным и минимальным элементов массива.
 
