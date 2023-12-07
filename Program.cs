2819e42f1a9c62788606ed854b4f84cd165af242
Catherine <katrinker@bk.ru>
2023-12-07 15:57:47 +0700

    solved 1 task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+
+/*int [] array = new int[10];
+int count = 0;
+
+for(int i = 0; i<array.Length; i++)
+{
+    array[i]= new Random().Next(1,101);  
+    System.Console.Write(array[i]+ " ");
+
+    if (array[i]>=20 && array[i]<=90)
+    {
+count = count + 1;
+    }
+
+}
+    System.Console.Write(count);*/
+
+
+
+
